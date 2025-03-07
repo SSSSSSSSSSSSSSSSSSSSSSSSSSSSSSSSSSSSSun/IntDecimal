d702ae031d1fd897609e178830c8d4387d5602dc
SSun <90237785+SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSun@users.noreply.github.com>
2025-03-08 01:34:27 +0900

    초기 커밋.

---
+++ IntDecimal/IntDecimal.h
@@ -0,0 +1,23 @@
+#pragma once
+class IntDecimal
+{
+private:
+	int integerPart;		// 정수부
+	int decimalPart;		// 소수부 (0~999999)
+
+public:
+
+	IntDecimal();						// 0.0
+
+	IntDecimal(const short&);			// short로 생성				(~32768~32767)
+	IntDecimal(const unsigned short&);	// unsigned short로 생성	(0~65535)
+	IntDecimal(const int&);				// int로 생성				(~2147483648~2147483647)
+
+	IntDecimal(const float&);			// float로 생성				(단, 범위 초과시 -2147483648이나 2147483647로 표기/소수점 7자리에서 반올림)
+	IntDecimal(const double&);			// double로 생성			(단, 범위 초과시 -2147483648이나 2147483647로 표기/소수점 7자리에서 반올림)
+
+	IntDecimal(const IntDecimal&);
+
+
+};
+
